f7e886de8a8b7a352a86689bd774ffb663f996e2
Willian Novais <willian.novais@hotmail.com>
2021-06-04 10:24:07 -0300

    Primeiro commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWill = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtWill
+            // 
+            this.txtWill.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWill.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtWill.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtWill.Location = new System.Drawing.Point(12, 12);
+            this.txtWill.Multiline = true;
+            this.txtWill.Name = "txtWill";
+            this.txtWill.Size = new System.Drawing.Size(776, 426);
+            this.txtWill.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtWill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWill;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static bool executar = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void NovaThread()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            executar = true;
+
+            for (int i = 1; i > 0; i++)
+            {
+                SendKeys.SendWait(i.ToString());
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                if (executar == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            executar = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            Thread t = new Thread(NovaThread);
+            txtWill.Text += t + Environment.NewLine;
+            t.Start();
+        }
+    }
+}
